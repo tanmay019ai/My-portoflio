be867b4f9e5f421141dbfc96affef2b8f5bf358d
tanmay019ai <tanmaysr019@gmail.com>
2025-07-19 15:41:36 +0530

    error 404 fix-122

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from "react"
+import React, { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import { Decal, Float, OrbitControls, Preload, useTexture } from "@react-three/drei";
 import CanvasLoader from "../Loader";
@@ -11,25 +11,35 @@ const isAndroid = () => {
 
 const Ball = (props) => {
   const [decal] = useTexture([props.imgUrl]);
+  const isAndroidDevice = isAndroid();
 
   return (
     <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
       <ambientLight intensity={0.25} />
       <directionalLight position={[0, 0, 0.05]} />
       <mesh castShadow receiveShadow scale={2.75}>
-        <icosahedronGeometry args={[1, 1]} />
-        <meshStandardMaterial
-          color="#fff8eb"
-          polygonOffset
-          polygonOffsetFactor={-5}
-          flatShading
-        />
-        <Decal
-          position={[0, 0, 1]}
-          rotation={[2 * Math.PI, 0, 6.25]}
-          map={decal}
-          flatShading
-        />
+        {isAndroidDevice ? (
+          <>
+            <sphereGeometry args={[1, 32, 32]} />
+            <meshStandardMaterial color="#2a2a2a" />
+          </>
+        ) : (
+          <>
+            <icosahedronGeometry args={[1, 1]} />
+            <meshStandardMaterial
+              color="#fff8eb"
+              polygonOffset
+              polygonOffsetFactor={-5}
+              flatShading
+            />
+            <Decal
+              position={[0, 0, 1]}
+              rotation={[2 * Math.PI, 0, 6.25]}
+              map={decal}
+              flatShading
+            />
+          </>
+        )}
       </mesh>
       <OrbitControls enableZoom={false} />
     </Float>
@@ -37,29 +47,6 @@ const Ball = (props) => {
 };
 
 const BallCanvas = ({ icon }) => {
-  const isAndroidDevice = isAndroid();
-
-  if (isAndroidDevice) {
-    return (
-      <div style={{
-        width: "112px",
-        height: "112px",
-        borderRadius: "9999px",
-        backgroundColor: "#fff8eb",
-        boxShadow: "0 4px 6px rgba(0,0,0,0.1)",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-      }}>
-        <img
-          src={icon}
-          alt="icon"
-          style={{ width: "64px", height: "64px", objectFit: "contain" }}
-        />
-      </div>
-    );
-  }
-
   return (
     <Canvas frameloop="demand" gl={{ preserveDrawingBuffer: true }}>
       <Suspense fallback={<CanvasLoader />}>
