82555417e2c67cd7dc6cf4deeb82c9021324b0f9
tanmay019ai <tanmaysr019@gmail.com>
2025-07-19 15:32:27 +0530

    error 404 fix-99

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,46 +1,74 @@
-import React, {Suspense} from "react"
+import React, { Suspense } from "react"
 import { Canvas } from "@react-three/fiber";
-import { Decal, Float , OrbitControls, Preload, useTexture } from "@react-three/drei";
+import { Decal, Float, OrbitControls, Preload, useTexture } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
+// Only detect Android
+const isAndroid = () => {
+  if (typeof navigator === "undefined") return false;
+  return /Android/i.test(navigator.userAgent);
+};
 
 const Ball = (props) => {
- const [decal] = useTexture([props.imgUrl]);
+  const [decal] = useTexture([props.imgUrl]);
 
   return (
-   <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
-    <ambientLight intensity={0.25} />
-    <directionalLight position={[0, 0, 0.05]} />
-    <mesh castShadow receiveShadow scale={2.75}>
-      <icosahedronGeometry args={[1, 1]} />
-      <meshStandardMaterial color="#fff8eb" polygonOffset polygonOffsetFactor={-5} flatShading />
-      <Decal
-        position={[0, 0, 1]}
-        rotation={[2 * Math.PI, 0, 6.25]}
-        map={decal}
-        flatShading
-      />  
-    </mesh>
-    <OrbitControls enableZoom={false} />
-    
-   </Float>
-  )
-}
+    <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
+      <ambientLight intensity={0.25} />
+      <directionalLight position={[0, 0, 0.05]} />
+      <mesh castShadow receiveShadow scale={2.75}>
+        <icosahedronGeometry args={[1, 1]} />
+        <meshStandardMaterial
+          color="#fff8eb"
+          polygonOffset
+          polygonOffsetFactor={-5}
+          flatShading
+        />
+        <Decal
+          position={[0, 0, 1]}
+          rotation={[2 * Math.PI, 0, 6.25]}
+          map={decal}
+          flatShading
+        />
+      </mesh>
+      <OrbitControls enableZoom={false} />
+    </Float>
+  );
+};
+
 const BallCanvas = ({ icon }) => {
+  const isAndroidDevice = isAndroid();
+
+  if (isAndroidDevice) {
+    return (
+      <div style={{
+        width: "112px",
+        height: "112px",
+        borderRadius: "9999px",
+        backgroundColor: "#fff8eb",
+        boxShadow: "0 4px 6px rgba(0,0,0,0.1)",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}>
+        <img
+          src={icon}
+          alt="icon"
+          style={{ width: "64px", height: "64px", objectFit: "contain" }}
+        />
+      </div>
+    );
+  }
+
   return (
-    <Canvas
-     frameloop="demand"
-     
-      gl={{preserveDrawingBuffer: true}}
-    >
+    <Canvas frameloop="demand" gl={{ preserveDrawingBuffer: true }}>
       <Suspense fallback={<CanvasLoader />}>
-        <OrbitControls enableZoom={false}/>
-        <Ball imgUrl={icon}/>
+        <OrbitControls enableZoom={false} />
+        <Ball imgUrl={icon} />
       </Suspense>
       <Preload all />
     </Canvas>
-  );  
-}
+  );
+};
 
-
-export default BallCanvas
+export default BallCanvas;
